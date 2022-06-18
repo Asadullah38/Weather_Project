97218dabbb1ff85c084cb33b76f389271351b148
Asadullah38 <2020se10@student.uet.edu.pk>
2022-06-18 19:14:50 +0500

    weather project first commit

---
+++ express.js
@@ -0,0 +1,37 @@
+import express from "express";
+var app = express();
+var port = process.env.PORT || 8000;
+import fs, { readFileSync } from "fs";
+const homefile = fs.readFileSync("weather.html", "utf-8");
+import requests from "requests";
+
+
+function replaceVal(tempval, val) {
+    tempval = tempval.replace("{%temp%}", val.main.temp);
+    tempval = tempval.replace("{%status%}", val.weather[0].main);
+    tempval = tempval.replace("{%country%}", val.sys.country);
+    tempval = tempval.replaceAll("{%city%}", val.name);
+    tempval = tempval.replace("{%min%}", val.main.temp_min);
+    tempval = tempval.replace("{%max%}", val.main.temp_max);
+    return tempval;
+}
+
+
+app.get("/", (req, res) => {
+    var city = req.query.name;
+    requests(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=09218d9fb44bd06f3210271b29385683`)
+        .on('data', (chunk) => {
+            const obj = JSON.parse(chunk);
+            var arrdata = [obj];
+            const realtimedata = arrdata.map((val) => replaceVal(homefile, val)).join('');
+            res.write(realtimedata);
+
+        })
+        .on('end', (err) => {
+            if (err) return console.log('connection closed due to errors', err);
+
+            res.end();
+
+        });
+
+}).listen(port);
