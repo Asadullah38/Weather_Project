50b381a73853ed878d6fc1e1d6b1d75d5912326d
Asadullah38 <2020se10@student.uet.edu.pk>
2022-06-19 18:43:41 +0500

    fourth

--- express.js
+++ express.js
@@ -15,23 +15,34 @@ function replaceVal(tempval, val) {
     tempval = tempval.replace("{%max%}", val.main.temp_max);
     return tempval;
 }
+var a = 0;
 
 
+var city = "Sargodha";
 app.get("/", (req, res) => {
-    var city = req.query.name;
-    requests(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=09218d9fb44bd06f3210271b29385683`)
-        .on('data', (chunk) => {
-            const obj = JSON.parse(chunk);
-            var arrdata = [obj];
-            const realtimedata = arrdata.map((val) => replaceVal(homefile, val)).join('');
-            res.write(realtimedata);
+    if (city != "") {
 
-        })
-        .on('end', (err) => {
-            if (err) return console.log('connection closed due to errors', err);
 
-            res.end();
+        if (a === 0) {
+            req.query.name = "Lahore";
+            a++;
+        }
+        city = req.query.name;
 
-        });
+        requests(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=09218d9fb44bd06f3210271b29385683`)
+            .on('data', (chunk) => {
+                const obj = JSON.parse(chunk);
+                var arrdata = [obj];
+                const realtimedata = arrdata.map((val) => replaceVal(homefile, val)).join('');
+                res.write(realtimedata);
 
+            })
+            .on('end', (err) => {
+                if (err) return console.log('connection closed due to errors', err);
+                res.end();
+
+            });
+    } else {
+        alert("Enter City Name");
+    }
 }).listen(port);
